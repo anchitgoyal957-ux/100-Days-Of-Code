c95e773c03be6e49c299f4ecbf12ab72346e6fe0
anchitgoyal957-ux <anchitgoyal957@gmail.com>
2025-09-15 23:50:57 +0530

    Added new programs for conditional statements

---
+++ Conditional statements/code15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+    if (c >= 'A' && c <= 'Z')
+    {
+        printf("%c is an uppercase alphabet.\n", c);
+    }
+    else if (c >= 'a' && c <= 'z')
+    {
+        printf("%c is a lowercase alphabet.\n", c);
+    }
+    else if (c >= '0' && c <= '9')
+    {
+        printf("%c is a digit.\n", c);
+    }
+    else
+    {
+        printf("%c is a special character.\n", c);
+    }
+    return 0;
+}
